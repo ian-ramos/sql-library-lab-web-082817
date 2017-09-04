bd2cd3c182db3b2345c546fc6dcb1830fa6fe9e0
ian-ramos <ramos.ianc@gmail.com>
2017-09-03 22:08:54 -0400

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO series (title) VALUES ("series 1"), ("series 2");
+INSERT INTO books (title) VALUES ("book 1"), ("book 2"), ("book 3"), ("book 4"), ("book 5"), ("book 6");
+INSERT INTO characters (name) VALUES ("character 1"), ("character 2"), ("character 3"), ("character 4"), ("character 5"), ("character 6"), ("character 7"), ("character 8");
+INSERT INTO subgenres (name) VALUES ("subgenre 1"), ("subgenre 2");
+INSERT INTO authors (name) VALUES ("author 1"), ("author 2");
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2), (2,2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), (11, 11), (12, 12), (13, 13), (14, 14), (15, 15), (16, 16);
